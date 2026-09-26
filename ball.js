eb010d077571ea8fd6d617e75f23325bf738bb3b
durankarina <duran.karina@cetys.edu.mx>
2026-09-25 21:47:43 -0700

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,22 @@
+function Ball () {
+  this.x = 0;   
+  this.y = 0;   
+  this.color = "#ffff00";   
+  this.scaleX = 2;   
+  this.scaleY = 1;   
+  this.rotation = 0;   
+  this.pelota = new Image();   
+  this.pelota.src = "fish.png";   
+  this.radius = 75; 
+} 
+  
+Ball.prototype.draw = function (context) {
+  context.save();
+  context.translate(this.x, this.y);
+  context.rotate(this.rotation);
+  context.scale(this.scaleX, this.scaleY);
+  
+  context.drawImage(this.pelota,-this.radius, -this.radius, this.radius * 2, this.radius * 2);   
+  context.restore();
+   
+};
